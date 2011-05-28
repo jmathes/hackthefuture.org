2cac6485eebda6afc097f2a29cdddc626cf06d60
smcq <smcq@saucelabs.com>
2011-05-28 11:32:31 -0700

    new website acutually in git.

--- hacktehfuture/hackthefuture.py
+++ hacktehfuture/hackthefuture.py
@@ -6,7 +6,37 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 
 class MainPage(webapp.RequestHandler):
     def get(self):
-        self.response.out.write("Hello Big World!")
+        self.response.out.write("""
+
+<!DOCTYPE html>
+<html>
+<head>
+	
+<title>
+Hack The Future Mailing List Signup
+</title>
+
+<style type="text/css">
+*{margin:0;padding:0;}
+html{overflow:hidden;}
+#wufoo{position:absolute;width:100%;height:100%;}
+body{margin-bottom:-17px;}
+iframe{height:100%;width:100%;border:none;}
+</style>
+
+</head>
+
+<body>
+	
+<div id="wufoo">
+<iframe title="Hack The Future Mailing List Signup" frameborder="0" src="http://hackthefuture.wufoo.com/forms/z7x3k7/">
+<a href="http://hackthefuture.wufoo.com/forms/z7x3k7/" title="HTML form">Fill out my Wufoo form!</a>
+</iframe>
+</div>
+
+</body>
+</html>
+""")
 #        self.response.out.write("""
 #          <html>
 #            <body>
